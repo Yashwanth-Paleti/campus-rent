8ce24f95523e25369a8dd24ca9b3d9cf857c7b89
Yashwanth-Paleti <yashwanthpaleti2006@gmail.com>
2025-12-22 11:07:32 +0530

    Frontend auth pages working with routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,42 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Features from "./components/Features";
 import HowItWorks from "./components/HowItWorks";
 import Footer from "./components/Footer";
 
-function App() {
+import Login from "./pages/Login.jsx";
+
+import Register from "./pages/Register.jsx";
+
+
+function Home() {
   return (
     <>
-      <Navbar />
       <Hero />
       <Features />
       <HowItWorks />
-      <Footer />
     </>
+  );
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+
+      <Routes>
+        {/* Home / Landing Page */}
+        <Route path="/" element={<Home />} />
+
+        {/* Auth Pages */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+
+      <Footer />
+    </BrowserRouter>
   );
 }
 
